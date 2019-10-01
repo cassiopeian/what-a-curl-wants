48732f43f6b80e303e7c8e605bd2ccd9224a0c11
cassiopeia <cassiopeia.neely@gmail.com>
2019-10-01 16:29:54 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# what-a-curl-wants
+ 
---
+++ js/index.js
@@ -0,0 +1,36 @@
+
+// ****** INDEX ******
+
+$(document).ready(function(){
+  $('.quiz-button').hide();
+  $('.curl.one').animate({top: 0}, 4000, function(){
+    $('.quiz-button').show();
+  });
+  $('.curl.two').animate({top: 0}, 2000);
+  $('.curl.three').animate({top: 0}, 1000);
+  $('.curl.four').animate({top: 0}, 500);
+});
+
+// ****** STICKY NAV ******
+
+$(document).ready(function(){
+  var num = $('.nav').offset().top;
+  $(window).bind('scroll', function() {
+    if ($(window).scrollTop() > num) {
+      $('.nav').addClass('fixed');
+    } else {
+      $('.nav').removeClass('fixed');
+    }
+  });
+});
+
+// ****** ABOUT CAROUSEL ******
+
+$(document).ready(function(){
+  $('.test-three').hide();
+  $('.test-four').hide();
+  $('.test-one').animate({left: '-100%'}, 4000, function(){
+    $('.test-two').animate({left: '-100%'}, 4000)
+  };
+});
+

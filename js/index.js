d69f74b5d6a86e339615a98b9d47b8c9a980a22e
cassiopeia <cassiopeia.neely@gmail.com>
2019-10-16 11:40:28 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -41,27 +41,80 @@ $(window).on('resize', function(){
 
 // ****** QUIZ BUTTON CLICKS ******
 
+let userSelection = {
+  'protein': '',
+  'density': '',
+  'porosity': ''
+};
+
+let products = [
+  {
+    'name': 'CAROL\'S DAUGHTER SACRED TIARE RESTORING HAIR MASK',
+    'image': '<img class="product-img" src="images/products/carols-daughter-hair-mask.png" alt="CAROL\'S DAUGHTER SACRED TIARE RESTORING HAIR MASK (8 OZ.)">',
+    'porosity': 'high',
+    'density': 'high',
+    'protein': 'yes',
+
+  },
+  {
+    'name': 'AFRICAN PRIDE MOISTURE MIRACLE MOROCCAN CLAY & SHEA BUTTER HEAT-ACTIVATED MASQUE',
+    'image': '<img class="product-img" src="images/products/african-pride-masque.png" alt="AFRICAN PRIDE MOISTURE MIRACLE MOROCCAN CLAY & SHEA BUTTER HEAT-ACTIVATED MASQUE (12 OZ.)">',
+    'porosity': 'low',
+    'density': 'low',
+    'protein': 'no',
+
+  },
+  {
+    'name': 'AUNT JACKIE\'S CURLS & COILS FIX MY HAIR INTENSIVE REPAIR CONDITIONING MASQUE',
+    'image': '<img class="product-img" src="images/products/aunt-jackies-conditioning-masque.png" alt="AUNT JACKIE\'S CURLS & COILS FLAXSEED RECIPES FIX MY HAIR INTENSIVE REPAIR CONDITIONING MASQUE (15 OZ.)">',
+    'porosity': 'low',
+    'density': 'high',
+    'protein': 'yes',
+
+  },
+];
+
 $('.quiz-start').on('click', function(){
-  $('.quiz-welcome').hide();
-  $('.quiz-1').show();
+  // $('.quiz-welcome').hide();
+  // $('.quiz-1').show();
+  $('.questions.active').removeClass('active').next('.questions').addClass('active');
 });
-$('.options.a, .options.b').on('click', function(){
-  $('.quiz-1').hide();
-  $('.quiz-2').show();
+
+$('.questions .options').on('click', function(){
+  $('.questions.active').removeClass('active').next('.questions').addClass('active');
+  userSelection[$(this).attr('data-type')] = $(this).attr('data-value');
+  console.log(userSelection);
 });
-$('.options.c, .options.d').on('click', function(){
-  $('.quiz-2').hide();
-  $('.quiz-3').show();
-});
+
+// $('.options.a, .options.b').on('click', function(){
+//   // $('.quiz-1').hide();
+//   // $('.quiz-2').show();
+//   $('.questions.active').removeClass('active').next('.questions').addClass('active');
+//   // userSelection.poro = $(this).attr('data-type')
+// });
+// $('.options.c, .options.d').on('click', function(){
+//   // $('.quiz-2').hide();
+//   // $('.quiz-3').show();
+//   $('.questions.active').removeClass('active').next('.questions').addClass('active');
+// });
 
 // $('.go-back').on('click', function(){
 //     $('.quiz-1').hide();
 //     $('.quiz-welcome').show();
 // });
 
-// $('.go-back').on('click', function(){
-//   $('.active').previous('section').addClass('active').next('section').removeClass('active');
-// });
+$('.go-back').on('click', function(){
+  $('.questions.active').removeClass('active').prev('.questions').addClass('active');
+});
+
+
+$(".test").on("click", function(){
+  for (let num = 0; num < products.length; num++){
+    if (userSelection.protein === products[num].protein) {
+      console.log(products[num].name);
+    }
+  }
+});
 
 // $('.go-back').on('click', function(){
 //   $(this).hide().previous('section').show();
